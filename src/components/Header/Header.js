fb6b0fcd9484be5625680be49df2bb4b57570c2b
Vital Pro <vitalijprokofjev@vitalijs-mbp.home>
2020-09-01 14:32:04 +0100

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import './styles/Header.scss'
+
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar, IconButton } from '@material-ui/core'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img 
+                    alt="logo" 
+                    src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512">
+                </img>
+                <div className="header__input">
+                    <SearchIcon fontsize="large"/>
+                    <input type="text" placeholder="Search Faceclone"/>
+                </div>
+            </div>
+
+            <div className="header__mid">
+                <div className="header__option header__option--active">
+                    <HomeIcon />
+                </div>
+                <div className="header__option">
+                    <FlagIcon />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsIcon />
+                </div>
+                <div className="header__option">
+                    <StorefrontIcon />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon />
+                </div> 
+            </div>
+
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Someone Name</h4>
+
+                    <IconButton>
+                        <AddIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ForumIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <NotificationsActiveIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ExpandMoreIcon/>
+                    </IconButton>
+                </div>    
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBr6O5hPavfjOZi_Mh5miq9gOzvHr3DI2A",
+    authDomain: "facebook-clone-41c6e.firebaseapp.com",
+    databaseURL: "https://facebook-clone-41c6e.firebaseio.com",
+    projectId: "facebook-clone-41c6e",
+    storageBucket: "facebook-clone-41c6e.appspot.com",
+    messagingSenderId: "488976849881",
+    appId: "1:488976849881:web:79402688b811770d2857c8",
+    measurementId: "G-FTQ6TQLE9K"
+  }
